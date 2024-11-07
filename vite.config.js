fdb1e46daedefae61c1ee62ea8ac20831c3385b3
KRUSHANG SAVALIYA <krushangsavaliya45@gmail.com>
2024-11-07 16:52:56 +0530

    in production

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", // change output directory to 'build'
+    outDir: "dist", // change output directory to 'build'
   },
 });
